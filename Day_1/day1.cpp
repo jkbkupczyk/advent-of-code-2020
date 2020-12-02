0312b3b694d415b697217d85e96b15a23b90fbf6
Jakub Kupczyk <69753899+jkbkupczyk@users.noreply.github.com>
2020-12-02 15:46:39 +0100

    Changed file name to day1

--- Day_1/day1.cpp
+++ Day_1/day1.cpp
@@ -1,28 +1,64 @@
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <vector>
 
-int populate(string filename){
+std::vector<int> populateVector(std::string fileName) {
 
-    ifstream file;
-    int num, next;
+	std::ifstream file;
 
-    file.open(filename);
+	std::vector<int> data;
+	int temp;
 
-    if(file.good()){
+	file.open(fileName);
 
-        while(file >> num){
-            std::cout << num << std::endl;
-        }
+	if (file.good()) {
+		while (file >> temp) {
+			data.push_back(temp);
+		}
 
-        file.close();
-    }
+		file.close();
+	}
 
-    return 0;
+	return data;
+}
+
+// Find the two entries that sum to 2020
+int find2Entries(std::vector<int> data, int sum) {
+
+	for (int i = 0; i < data.size(); i++) {
+		for (int j = i + 1; j < data.size() - 1; j++) {
+			if (data[i] + data[j] == sum) {
+				return data[i] * data[j];
+			}
+		}
+	}
+
+	return 0;
+}
+
+// Find the three entries that sum to 2020
+int find3Entries(std::vector<int> data, int sum) {
+
+	for (int i = 0; i < data.size(); i++) {
+		for (int j = i + 1; j < data.size(); j++) {
+			for (int k = j + 1; k < data.size(); k++) {
+				if (data[i] + data[j] + data[k] == sum) {
+					return data[i] * data[j] * data[k];
+				}
+			}
+		}
+	}
+
+	return 0;
 }
 
 int main()
 {
-    populate("input.txt");
+	std::vector<int> entries = populateVector("input.txt");
 
-    return 0;
+	std::cout << "2 entries: " << find2Entries(entries, 2020) << std::endl;
+	std::cout << "3 entries: " << find3Entries(entries, 2020) << std::endl;
+	
+	return 0;
 }
