27e7f999d4f121f30aff7a948dfc4e3a19a0fb3c
Jakub Kupczyk <69753899+jkbkupczyk@users.noreply.github.com>
2020-12-02 17:03:41 +0100

    Day 2 🕑

--- Day_2/day2.cpp
+++ Day_2/day2.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <vector>
 
+// Struct that represent input
 struct PassPolicy {
 	int minChars;
 	int maxChars;
@@ -10,6 +11,7 @@ struct PassPolicy {
 	std::string pass;
 };
 
+// Mapping input to struct
 std::vector<PassPolicy> populateVector(std::string fileName){
 
 	std::ifstream file;
@@ -39,27 +41,44 @@ std::vector<PassPolicy> populateVector(std::string fileName){
 	return data;	
 }
 
+// Task 1
 bool validate(PassPolicy passPolicy) {
 
 	int count = 0;
 
 	for (int i = 0; i < passPolicy.pass.size(); i++) {
-		if (passPolicy.pass.at(i) == passPolicy.letter)
+		if (passPolicy.pass.at(i) == passPolicy.letter) {
 			count++;
+		}
 	}
 
-	if (count >= passPolicy.minChars && count <= passPolicy.maxChars)
+	if (count >= passPolicy.minChars && count <= passPolicy.maxChars) {
 		return true;
+	}
 
 	return false;
 }
 
-int numberOfValid(std::vector<PassPolicy> vector) {
+// Task 2
+bool validate2(PassPolicy passPolicy) {
+	int min = passPolicy.minChars - 1,
+		max = passPolicy.maxChars - 1;
+
+	if ((passPolicy.pass.at(min) == passPolicy.letter || passPolicy.pass.at(max) == passPolicy.letter) 
+		&& passPolicy.pass.at(min) != passPolicy.pass.at(max)) {
+		return true;
+	}
+
+	return false;	
+}
+
+// Return number of passwords that met specific criteria
+int numberOfValid(std::vector<PassPolicy> vector, bool(*validator)(PassPolicy)) {
 
 	int count = 0;
 
 	for (int i = 0; i < vector.size(); i++) {
-		if (validate(vector[i])) {
+		if (validator(vector[i])) {
 			count++;
 		}
 	}
@@ -71,7 +90,8 @@ int main() {
 
 	std::vector<PassPolicy> passwords = populateVector("input.txt");
 
-	std::cout << "Valid passwords: " << numberOfValid(passwords) << std::endl;
+	std::cout << "1. Valid passwords (1st criteria): " << numberOfValid(passwords, validate) << std::endl;
+	std::cout << "2. Valid passwords (2nd criteria): " << numberOfValid(passwords, validate2) << std::endl;
 
 	return 0;
 }
